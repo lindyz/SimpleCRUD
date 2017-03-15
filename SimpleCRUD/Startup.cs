cc73f78d18be51716c807fdfedee602ee29b2ecf
lindyz <mkz.attorneyatlaw@hotmail.com>
2017-03-15 05:44:59 -0400

    Add project files.

---
+++ SimpleCRUD/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SimpleCRUD.Startup))]
+namespace SimpleCRUD
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
